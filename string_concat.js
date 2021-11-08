c3a15e3223384795511b1c0d22efb471dd4211dc
adnaan-maulid <adnaan9657@gmail.com>
2021-11-08 07:15:31 +0300

    Made some changes

--- string_concat.js
+++ string_concat.js
@@ -1,4 +1,7 @@
-let first_name = "Aliyah"
-let Last_name = "Maulid"
+function concat(first_name,Last_name){
 let full_name = first_name + Last_name
 console.log(full_name)
+}
+let first_name = "Aliyah"
+let Last_name = "Maulid"
+concat(first_name,Last_name)
